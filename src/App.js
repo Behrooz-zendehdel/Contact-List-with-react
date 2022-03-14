b5008893421fc2479542c3a1c097e777879cc5cb
behrooz <behrooz.zendedel1992@gmail.com>
2022-03-14 18:00:48 +0330

    add addHandler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import { useState } from 'react';
 import './App.css';
+import AddContact from './components/AddContact/AddContact';
+import ContactList from './components/ContactList/ContactList';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+
+  const addContactHandler = (contact) => {
+
+    setContacts([...contacts, { id: Math.ceil(Math.random()* 100) , ...contact }]);
+  }
+  const deleteContactHandler = () => {
+    console.log('clicked')
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <main className="App">
+      <h1>Contact App</h1>
+      <section>
+        <AddContact addContactHandler={addContactHandler} />
+      </section>
+      <section>
+        <ContactList contacts={contacts} onDelete={deleteContactHandler} />
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,21 @@
+const ContactList = ({ contacts, onDelete }) => {
+
+    return (
+        <>
+            {
+                contacts.map((contact) => {
+                    const { name, email, id } = contact
+                    return (
+                    <div key={id}>
+                        <p>name : {name}</p>
+                        <p>email : {email}</p>
+                        <button onClick={() => onDelete(id)}>delete</button>
+                    </div>
+    )
+})
+            }
+        </>
+    );
+}
+
+export default ContactList;
